a46896c128d21561807aa055e991c8ed85177949
Joe <erreyjoe@gmail.com>
2022-08-02 20:32:01 +0100

    changed story output method, and resting functionality.

--- main.py
+++ main.py
@@ -8,29 +8,58 @@ from rich.prompt import Prompt
 from rich.progress import track
 import os
 
+dev = True
+
 os.system("cls")
-rich.print(Panel("Twilight",subtitle_align="center" ,subtitle="A turn-based card game"))
-print()
 
-rich.print("[cyan]Messenger[/cyan]: Hello my liege. I am a messenger from an enemy kingdom. Can I know whose land I am residing in?")
-player = Player(Prompt.ask("[green]You[/green]: Of course, messenger, I am King").capitalize())
-rich.print(f"[cyan]Court Squire [/cyan]: Oh King {player.getName()}! Our king has great honour and wants to inform you to prepare for war over this very land.")
-rich.print("[green]You[/green]: And which king might that be?")
-bot = Player(Prompt.ask(f"[cyan]Court Squire [/cyan]: Our king is called").capitalize())
-rich.print(f"[green]You[/green]: Then tell King {bot.getName()} to prepare for war...")
+if not dev:
+    rich.print(Panel(f"Introduction",subtitle_align="center", title="Twilight", subtitle="A turn-based card game"))
+    print()
 
-time.sleep(4)
+    rich.print("[cyan]Messenger[/cyan]: ")
+    ut.printout("Hello my liege. I am a messenger from an enemy kingdom. Can I know whose land I am residing in?")
+    time.sleep(2)
+    print()
 
-for i in track(range(50), description="Preparing for battle..."):
-    time.sleep(0.1)
+    rich.print("[green]You[/green]: ")
+    ut.printout("Of course, messenger, I am King", nl=False)
+    player = Player(Prompt.ask().capitalize())
+    time.sleep(2)
+    print()
 
+    rich.print(f"[cyan]Court Squire [/cyan]: ")
+    ut.printout(f"Oh King {player.getName()}! Our king has great honour and wants to inform you to prepare for war over this very land.")
+    time.sleep(2)
+    print()
+
+    rich.print("[green]You[/green]: ")
+    ut.printout("And which king might that be?")
+    time.sleep(2)
+    print()
+
+    rich.print("[cyan]Court Squire [/cyan]: ")
+    ut.printout("Our king is called", nl=False)
+    bot = Player(Prompt.ask().capitalize())
+    time.sleep(2)
+    print()
+
+    rich.print("[green]You[/green]: ")
+    ut.printout(f"Then tell King {bot.getName()} to prepare for war...")
+
+    time.sleep(4)
+
+    for i in track(range(50), description="Preparing for battle..."):
+        time.sleep(0.1)
+else:
+    player = Player("Player")
+    bot = Player("Bot")
 turn = 0
 
 history = []
 
 while not player.isLost() and not bot.isLost():
     os.system("cls")
-    rich.print(Panel("Twilight",subtitle_align="center" ,subtitle="A turn-based card game"))
+    rich.print(Panel(f"Day {int((turn+2)/2)}",subtitle_align="center", title="Twilight", subtitle="A turn-based card game"))
     print()
     
     player.addMana((turn-1)%3 + 1) if turn > 0 else None
@@ -44,6 +73,8 @@ while not player.isLost() and not bot.isLost():
     for i in history[-5:]:
         rich.print(i)
         
+    print()
+        
     rich.print(f"Play a card to {'attack' if turn%2 == 0 else 'defend against'} [red]{bot.getName()}[/red]:")
 
     while (not (cardPlayed := player.getCard(input(">> ")))):
@@ -52,48 +83,68 @@ while not player.isLost() and not bot.isLost():
     botCard = bot.getRandomCard()
     
     if cardPlayed["NAME"] == "Rest" and botCard["NAME"] == "Rest":
-        rich.print(f"[green]{player.getName()}[/green] and [red]{bot.getName()}[/red] have agreed to a temporary peace treaty...")
-        history.append(f"[green]{player.getName()}[/green] and [red]{bot.getName()}[/red] have agreed to a temporary peace treaty...")
+        rich.print(f"[green]{player.getName()}[/green] and [red]{bot.getName()}[/red] have agreed to a temporary peace treaty, and both parties gain health...")
+        party = player.getHand()[1:] + bot.getHand()[1:]
+        
+        for i in party:
+                i["HP"] += 2
+        
+        history.append(f"[green]{player.getName()}[/green] and [red]{bot.getName()}[/red] have agreed to a temporary peace treaty, and both parties gain health...")
         turn += 1
         time.sleep(4)
         continue
     
-    if turn%2 == 1 and cardPlayed["NAME"] == "Rest":
+    if cardPlayed["NAME"] == "Rest":
         party = player.getHand()[1:]
-        for i in party:
-            ut.attack(botCard, i, turn)
-            if i["HP"] <= 0:
-                player.addToGraveyard(i)
-        
-        rich.print(f"[red]{bot.getName()}[/red] attacks the entire party while [green]{player.getName()}[/green] was resting for [white on red]{botCard['DMG']}[/white on red] damage!")
-        history.append(f"[red]{bot.getName()}[/red] attacks the entire party while [green]{player.getName()}[/green] was resting for [white on red]{botCard['DMG']}[/white on red] damage!")
-        turn += 1
-        time.sleep(4)
-        continue
+        if turn%2 == 1:
+            
+            partyLost = []
 
-    if turn%2 == 0 and botCard["NAME"] == "Rest":
-        party = bot.getHand()[1:]
-        for i in party:
-            ut.attack(cardPlayed, i, turn)
-            if i["HP"] <= 0:
-                bot.addToGraveyard(i)
+            for i in party:
+                ut.attack(botCard, i, turn, restAttack=True)
+                if i["HP"] <= 0:
+                    partyLost.append(i["NAME"])
+                    player.addToGraveyard(i)
+
+            rich.print(f"[red]{bot.getName()}[/red] attacks the entire party while [green]{player.getName()}[/green] was resting for [white on red]{botCard['DMG']}[/white on red] damage! And killed {[name for name in partyLost]}!")
+            history.append(f"[red]{bot.getName()}[/red] attacks the entire party while [green]{player.getName()}[/green] was resting for [white on red]{botCard['DMG']}[/white on red] damage! And killed {[name for name in partyLost]}!")
+            turn += 1
+            time.sleep(4)
+            continue        
+        else:
+            for i in party:
+                i["HP"] += 2
         
-        rich.print(f"[green]{player.getName()}[/green] attacks the entire party while [red]{bot.getName()}[/red] was resting for [white on red]{cardPlayed['DMG']}[/white on red] damage!")
-        history.append(f"[green]{player.getName()}[/green] attacks the entire party while [red]{bot.getName()}[/red] was resting for [white on red]{cardPlayed['DMG']}[/white on red] damage!")
-        turn += 1
-        time.sleep(4)
-        continue
+        
+    
+    if botCard["NAME"] == "Rest":
+        party = bot.getHand()[1:]
+        if turn%2 == 0:
+            partyLost = []
+        
+            for i in party:
+                ut.attack(cardPlayed, i, turn, restAttack=True)
+                if i["HP"] <= 0:
+                    partyLost.append(i["NAME"])
+                    bot.addToGraveyard(i)
+                turn += 1
+            time.sleep(4)
+            continue
+        else:
+            for i in party:
+                i["HP"] += 2
+        
     
     currentMessage = ""
     
     if turn % 2 == 0:
-        currentMessage = ut.attack(cardPlayed, botCard, turn) if cardPlayed["NAME"] != "Rest" else f"{player.getName()} [green]rested[/green] their weary bones."
+        currentMessage = ut.attack(cardPlayed, botCard, turn) if cardPlayed["NAME"] != "Rest" else f"{player.getName()} [green]rested[/green] their weary bones and the party gains health."
         if botCard["HP"] <= 0 and botCard["NAME"] != "Rest":
             print(f"{bot.getName()} lost {botCard['NAME']}")
             history.append(f"{bot.getName()} lost {botCard['NAME']}")
             bot.addToGraveyard(botCard)
     else:
-        currentMessage = ut.attack(botCard, cardPlayed, turn) if botCard["NAME"] != "Rest" else f"{bot.getName()} [red]rested[/red] their weary bones."
+        currentMessage = ut.attack(botCard, cardPlayed, turn) if botCard["NAME"] != "Rest" else f"{bot.getName()} [red]rested[/red] their weary bones and the party gains health."
         if cardPlayed["HP"] <= 0 and cardPlayed["NAME"] != "Rest":
             print(f"{player.getName()} lost {botCard['NAME']}")
             history.append(f"{player.getName()} lost {botCard['NAME']}")
@@ -106,12 +157,21 @@ while not player.isLost() and not bot.isLost():
     turn += 1
     
 if player.isLost():
-    rich.print("You have fallen..")
+    ut.printout("You have fallen..")
     time.sleep(6)
-    rich.print(f"Your kingdom falls around you as {bot.getName()} takes your crown and proclaims himself the ruler of the land.")
-    rich.print("You fought your best and it was all for naught...")
+    ut.printout(f"Your kingdom falls around you as {bot.getName()} takes your crown and proclaims himself the ruler of the land.")
+    ut.printout("You fought your best and it was all for naught...")
 else:
-    rich.print("You have risen..")
+    ut.printout("You have risen..")
     time.sleep(6)
-    rich.print(f"You take {bot.getName()}'s crown and place it on your head. You have expanded your kingdom, and built it on the land of your enemies.")
-    rich.print("There is no limit to your power...")
+    ut.printout(f"You take {bot.getName()}'s crown and place it on your head. You have expanded your kingdom, and built it on the land of your enemies.")
+    ut.printout("There is no limit to your power...")
+
+"""
+To implement:
+    Spell stage every 5 days?    
+    Damages ranges, misses, critical hits etc: Done
+    Ability to swap cards
+    Ability to gain new cards
+    Ability to heal cards
+"""
--- utilities.py
+++ utilities.py
@@ -1,7 +1,8 @@
 import rich
+import random
 import time
 
-def attack(card1: dict, card2: dict, turn: int):
+def attack(card1: dict, card2: dict, turn: int, restAttack: bool = False):
     dialogue = {
         "Rest": "You shouldnt see this",
         "Brute": "punches",
@@ -15,17 +16,33 @@ def attack(card1: dict, card2: dict, turn: int):
         "Archer": "shoots"    
         }
     
-    card2["HP"] -= card1["DMG"]
+    if restAttack:
+        card2["HP"] -= card1["DMG"]
+        return
     
     primaryColour, secondaryColour = ("green", "red") if turn % 2 == 0 else ("red", "green")
+    damage = random.choice(list(range(card1["DMG"]-1, card1["DMG"]+2)))
     
-    return f"The [{primaryColour} italic] {card1['NAME']} [/{primaryColour} italic] {dialogue[card1['NAME']]} the [{secondaryColour} italic] {card2['NAME']}[/{secondaryColour} italic] for [white on red]{card1['DMG']}[/white on red] damage!"
+    typeOfHit = random.choice(["miss"] + ["normal"]*3 + ["critical"])
+    message = None
     
-def printout(s: str, colour: str = "black"):
-    sl = f"[{colour}]hey"
-    rich.print(sl)
+    match typeOfHit:
+        case "miss":
+            message = f"[{primaryColour}]{card1['NAME']}[/{primaryColour}] missed the [{secondaryColour}]{card2['NAME']}[/{secondaryColour}]!"
+        case "normal":
+            message = f"The [{primaryColour} italic] {card1['NAME']} [/{primaryColour} italic] {dialogue[card1['NAME']]} the [{secondaryColour} italic] {card2['NAME']}[/{secondaryColour} italic] for [white on red]{damage}[/white on red] damage!"
+            card2["HP"] -= damage        
+        case "critical":
+            damage = int(damage*1.5)
+            message = f"The [{primaryColour} italic] {card1['NAME']} [/{primaryColour} italic] {dialogue[card1['NAME']]} the [{secondaryColour} italic] {card2['NAME']}[/{secondaryColour} italic] for [white on red][underline]CRTITICAL[/underline] {damage}[/white on red] damage!"
+            card2["HP"] -= damage  
+       
+    return message
+
+def printout(s: str, nl: bool = True):
     for c in s:
         rich.print(c, end="", flush=True)
-        time.sleep(0.8/len(s))
+        time.sleep(0.05)
+    print() if nl else None
         
 
